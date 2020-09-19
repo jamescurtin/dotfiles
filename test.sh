3607a2433b1b67bbafc00ffd604500eb2026ac68
James Curtin <jameswcurtin@gmail.com>
2020-09-19 13:30:19 -0400

    Add Github actions

--- test.sh
+++ test.sh
@@ -1,3 +1,56 @@
 #!/usr/bin/env bash
 
-shellcheck -- **/*.sh
+export SHELLCHECK_OPTS="-e SC2034 --shell=bash"
+
+shebangregex="^#! */[^ ]*/(env *)?[abkz]*sh"
+
+filepaths=()
+excludes=( ! -path *./.git/* )
+excludes+=( ! -path *.go )
+excludes+=( ! -path */mvnw )
+excludes+=( ! -path */meta/dotbot/* )
+
+readarray -d '' filepaths < <(find . -type f "${excludes[@]}" \
+    '(' \
+    \
+    -name '*.bash' \
+    -o -name '.bashrc' \
+    -o -name 'bashrc' \
+    -o -name '.bash_aliases' \
+    -o -name '.bash_completion' \
+    -o -name '.bash_login' \
+    -o -name '.bash_logout' \
+    -o -name '.bash_profile' \
+    -o -name 'bash_profile' \
+    -o -name '*.ksh' \
+    -o -name 'suid_profile' \
+    -o -name '*.zsh' \
+    -o -name '.zlogin' \
+    -o -name 'zlogin' \
+    -o -name '.zlogout' \
+    -o -name 'zlogout' \
+    -o -name '.zprofile' \
+    -o -name 'zprofile' \
+    -o -name '.zsenv' \
+    -o -name 'zsenv' \
+    -o -name '.zshrc' \
+    -o -name 'zshrc' \
+    -o -name '*.sh' \
+    -o -path '*/.profile' \
+    -o -path '*/profile' \
+    -o -name '*.shlib' \
+       ')'\
+    \
+    -print0)
+
+readarray -d '' tmp < <(find . "${excludes[@]}" -type f ! -name '*.*' -print0)
+for file in "${tmp[@]}"; do
+    head -n1 "$file" | grep -Eqs "$shebangregex" || continue
+    filepaths+=("$file")
+done
+
+echo "::debug:: shellcheck ${filepaths[*]}"
+shellcheck "${filepaths[@]}"
+
+echo "Running yamllint..."
+yamllint .
